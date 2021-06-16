01ee83e4c5ec0a453774e7c21374de7e57dcf11a
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-06-17 00:24:12 +0530

    connected to database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose')
 
 const app = express();
 const port = 3000;
@@ -9,25 +10,80 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [];
+//Connection URL
+mongoose.connect(`mongodb://localhost:27017/todolistDB`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add item.']
+    },
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+// const item1 = new Item({
+//     name: 'Welcome to Todo App'
+// });
+
+// const item2 = new Item({
+//     name: 'Type an item'
+// });
+
+// const item3 = new Item({
+//     name: '<-- Hit to delete'
+// });
+
+// const items = [item1, item2, item3];
 
 app.get('/', function(req, res) {
 
     const day = date();
-    res.render('list', { kindOfDay: day, newItems: items });
+
+    Item.find(function(err, foundItems) {
+        console.log(foundItems)
+        res.render('list', { kindOfDay: day, newItems: foundItems });
+        // if (foundItems.length == 0) {
+        //     Item.insertMany(items, function(err) {
+        //         if (err) {
+        //             console.log(err)
+        //         } else {
+        //             console.log('Successfully added items')
+        //             res.redirect('/');
+        //         }
+        //     });
+        // } else {
+        //     console.log(foundItems)
+        //     res.render('list', { kindOfDay: day, newItems: foundItems });
+        // }
+    });
+
 });
 
 app.post('/', function(req, res) {
-    item = req.body.newItem;
-    items.push(item);
+    newItem = req.body.newItem;
 
-    res.redirect('/');
+    const item = new Item({
+        name: newItem
+    });
+
+    item.save(function() {
+        res.redirect('/');
+    });
+
 });
 
 app.get('/deleteItem/:id', function(req, res) {
 
-    const ind = req.params.id;
-    items.splice(ind, 1);
+    const id = req.params.id;
+    console.log(id)
+    Item.deleteOne({ _id: id }, function(err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('deleted successfully')
+        }
+    })
 
     res.redirect('/');
 });
